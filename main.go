8841fd6e78e9cb4434c160277208f7c836e3b3f4
Ethan Mosbaugh <emosbaugh@gmail.com>
2017-04-18 13:20:25 -0700

    fixes

--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
-	"time"
-  "sync"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/cactus/go-statsd-client/statsd"
 )
@@ -20,7 +20,7 @@ var (
 	httpClient        *http.Client
 	curStatsdEndpoint = ""
 	newStatsdEndpoint = ""
-  endPointMutex     sync.Mutex
+	endPointMutex     sync.Mutex
 )
 
 func main() {
@@ -35,14 +35,14 @@ func main() {
 	go Ping("8.8.8.8")
 	go Ping("8.8.4.4")
 	go Disk("/")
-  // go generateGauge("myapp.mygauge1", 555)
-  // go generateGauge("myapp.mygauge2", 9999)
+	// go generateGauge("myapp.mygauge1", 555)
+	// go generateGauge("myapp.mygauge2", 9999)
 
-  <- make(chan interface{})
+	<-make(chan interface{})
 }
 
 func generateGauge(name string, max int) {
-  delay := func() {}
+	delay := func() {}
 	for {
 		delay()
 		delay = func() { <-time.After(2 * time.Second) }
@@ -57,15 +57,15 @@ func sendGauge(name string, val int64) {
 		return
 	}
 
-  //fmt.Println("Sending gauge")
+	//fmt.Println("Sending gauge")
 	statsdClient.Gauge(name, val, 1)
 }
 
 func maybeReplaceStatsdClient() {
-  endPointMutex.Lock()
-  defer endPointMutex.Unlock()
+	endPointMutex.Lock()
+	defer endPointMutex.Unlock()
 
-	if curStatsdEndpoint == newStatsdEndpoint {
+	if statsdClient != nil && curStatsdEndpoint == newStatsdEndpoint {
 		return
 	}
 
@@ -86,7 +86,7 @@ func maybeReplaceStatsdClient() {
 		return
 	}
 
-  fmt.Println("Made new statsd client, pointing at", curStatsdEndpoint)
+	fmt.Println("Made new statsd client, pointing at", curStatsdEndpoint)
 	statsdClient = client
 }
 
@@ -104,7 +104,7 @@ func monitorStatsdEndpoint() {
 		}
 
 		url := fmt.Sprintf("%s/console/v1/option?name=statsd.endpoint", apiEndpoint)
-    fmt.Printf("Calling %s\n", url)
+		fmt.Printf("Calling %s\n", url)
 		resp, err := httpClient.Get(url)
 		if err != nil {
 			fmt.Printf("%s: ERROR: %v\n", url, err)
@@ -113,11 +113,11 @@ func monitorStatsdEndpoint() {
 
 		if resp.StatusCode != http.StatusOK {
 			fmt.Printf("%v: BAD STATUS: %v\n", resp.StatusCode, err)
-      continue
+			continue
 		}
 
 		body, err := ioutil.ReadAll(resp.Body)
-    resp.Body.Close()
+		resp.Body.Close()
 		if err != nil {
 			fmt.Printf("Could not read responce body: %v\n", err)
 			continue
--- ping.go
+++ ping.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-  "net"
-  "fmt"
-  "time"
+	"fmt"
+	"net"
+	"time"
 
-  "github.com/tatsushid/go-fastping"
+	"github.com/tatsushid/go-fastping"
 )
 
 func Ping(server string) {
-  fmt.Println("Pinging ", server)
-  graphiteKey := fmt.Sprintf("ping.%s", cleanKey(server))
+	fmt.Println("Pinging ", server)
+	graphiteKey := fmt.Sprintf("ping.%s", cleanKey(server))
 
-  pinger := fastping.NewPinger()
-  ra, err := net.ResolveIPAddr("ip4:icmp", server)
-  if err != nil {
-    fmt.Println("Failed to resolve %s: %v", server, err)
-    return
-  }
-  pinger.AddIPAddr(ra)
-  pinger.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
-    sendGauge(graphiteKey, rtt.Nanoseconds() / 1000)
-  }
-  pinger.OnIdle = func() {
-    // no-op
-  }
-  pinger.RunLoop()
+	pinger := fastping.NewPinger()
+	ra, err := net.ResolveIPAddr("ip4:icmp", server)
+	if err != nil {
+		fmt.Println("Failed to resolve %s: %v", server, err)
+		return
+	}
+	pinger.AddIPAddr(ra)
+	pinger.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
+		sendGauge(graphiteKey, rtt.Nanoseconds()/1000)
+	}
+	pinger.OnIdle = func() {
+		// no-op
+	}
+	pinger.RunLoop()
 }
